52ae050d48c71b835129289c6bbc5ac85b6cb4d9
MariFarias <marinafarias.mk@gmail.com>
2022-01-11 14:07:27 -0300

    Adição de serviços de genero e End Point da API

---
+++ src/app/compartilhado/app-material/app-material.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+@NgModule({
+  exports: [
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatProgressSpinnerModule
+  ]
+})
+export class AppMaterialModule { }
